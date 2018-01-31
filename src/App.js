442cc15472cdc6d9e128c9cab44b8be77070e7b1
Alexandr <zhmurik@yandex.ru>
2018-01-31 18:32:23 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import data from './data.js';
+
+class App extends Component {
+  render() {
+    console.log('data', data);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
