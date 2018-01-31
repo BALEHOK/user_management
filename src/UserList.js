bf860c979796145dedfaf58f76d530762ea3e3a3
Alexandr <zhmurik@yandex.ru>
2018-01-31 19:18:21 +0700

    filter users

--- src/UserList.js
+++ src/UserList.js
@@ -4,12 +4,39 @@ import userRepository from './userRepository';
 import UserItem from './UserItem';
 
 class UserList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onFilterChanged = this.onFilterChanged.bind(this);
+
+    this.state = {
+      filter: ''
+    }
+  }
+
+  onFilterChanged(event) {
+    this.setState({ filter: event.target.value });
+  }
+
+  // get users and apply case insensitive filtering
+  getUsers() {
+    const filter = this.state.filter.toLowerCase();
+    let users = userRepository.getUsers();
+    if (filter !== '') {
+      users = users.filter(u => u.first_name.toLowerCase().indexOf(filter) !== -1);
+    }
+
+    return users;
+  }
+
   render() {
-    const users = userRepository.getUsers();
+    const users = this.getUsers();
 
     return (
       <div>
-        <div>search placeholder</div>
+        <div>
+          <input type="text" placeholder="filter by first name" onChange={this.onFilterChanged} />
+        </div>
         <ul>
           {users.map(u => <li key={u.id}><UserItem user={u} /></li>)}
         </ul>
